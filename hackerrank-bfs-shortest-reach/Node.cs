80410bd71410d5ca0b3a823c8b761c0a35289e21
Guilherme Ramos Mendes <guilhermerm.ciandt@sulamerica.com.br>
2020-04-14 09:17:30 -0300

    Solution

---
+++ hackerrank-bfs-shortest-reach/Node.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace hackerrank_bfs_shortest_reach
+{
+    public class Node
+    {
+
+        private readonly IList<Node> _neighbors;
+
+        public Node(int id)
+        {
+            Id = id;
+            Distance = -1;
+            _neighbors = new List<Node>();
+        }
+
+        public int Id { get; private set; }
+        public int Distance { get; private set; }
+        public IReadOnlyCollection<Node> Neighbors => _neighbors.ToArray();
+
+        public void ConnectTo(Node neighbor)
+        {
+            _neighbors.Add(neighbor);
+            neighbor.AddReferenceOfThisNodeToTheNeighboringNode();
+        }
+
+        public void AddReferenceOfThisNodeToTheNeighboringNode()
+        {
+            _neighbors.Add(this);
+        }
+
+        public void SetDistance(int distance)
+        {
+            Distance = distance;
+        }
+    }
+}
---
+++ hackerrank-bfs-shortest-reach/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace hackerrank_bfs_shortest_reach
+{
+    class Program
+    {
+
+        private static readonly int _edgeWeight = 6;
+
+        static void Main()
+        {
+            int numberOfQueries = int.Parse(Console.ReadLine());
+
+            for (int numberOfQuery = 1; numberOfQuery <= numberOfQueries; numberOfQuery++)
+            {
+                int[] numberOfNodesAndEdges = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+                int numberOfNodes = numberOfNodesAndEdges[0];
+                int numberOfEdges = numberOfNodesAndEdges[1];
+
+                List<int[]> connections = new List<int[]>();
+
+                for (int i = 0; i < numberOfEdges; i++)
+                {
+                    int[] connection = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+                    connections.Add(connection);
+                }
+
+                int sourceNode = int.Parse(Console.ReadLine());
+
+                Node[] nodes = new Node[numberOfNodes + 1];
+                nodes[0] = null;
+                for (int i = 1; i <= numberOfNodes; i++)
+                {
+                    nodes[i] = new Node(i);
+                }
+
+                connections.ForEach(connection =>
+                {
+                    Node nodeU = nodes[connection[0]];
+                    Node nodeV = nodes[connection[1]];
+                    nodeU.ConnectTo(nodeV);
+                });
+
+                FindDistances(nodes[sourceNode]);
+
+                for (int node = 1; node <= numberOfNodes; node++)
+                {
+                    if (node != sourceNode)
+                    {
+                        Console.Write($"{nodes[node].Distance} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FindDistances(Node start)
+        {
+            if (start == null)
+                return;
+
+            start.SetDistance(0);
+            List<Node> queue = new List<Node> { start };
+            while (queue.Count() != 0)
+            {
+                Node current = queue.First();
+                queue.Remove(current);
+                foreach (Node neighbor in current.Neighbors)
+                {
+                    if (WasTheNeighborVisited(neighbor))
+                    {
+                        neighbor.SetDistance(current.Distance + _edgeWeight);
+                        queue.Add(neighbor);
+                    }
+                }
+            }
+        }
+
+
+        private static bool WasTheNeighborVisited(Node neighbor)
+        {
+            return neighbor.Distance.Equals(-1);
+        }
+    }
+}
